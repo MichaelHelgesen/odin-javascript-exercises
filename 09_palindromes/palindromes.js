9451dfd24d0e5482256c65ee3f92ea19d9eb92fe
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-04 20:18:23 +0100

    Update function for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,13 @@
 const palindromes = function (word) {
 	const wordClean = word.replace(/[^A-Za-z0-9]/g,'').toUpperCase();
-	const wordToArray = wordClean.split("");
-	const wordLength = wordToArray.length;
-	for(i = wordLength - 1; i >= 0; i--){
-		if(wordClean[i] != wordClean[(wordLength - 1) - i]){
+	const wordInReverse = wordClean.split("").reverse().join("");
+	for(i = wordClean.length - 1; i >= 0; i--){
+		if(wordClean.charAt(i) != wordInReverse.charAt(i)){
 			return false;
 		}
 	}
 	return true;
 };
+
 // Do not edit below this line
 module.exports = palindromes;
