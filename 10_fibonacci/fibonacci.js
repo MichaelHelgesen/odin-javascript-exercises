bfc32ecd8f1da550441695e35e63ddd7bdbe06fb
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-04 22:51:59 +0100

    Create fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if( num == 0){
+		return 0;
+	} else if (num < 0){
+		return "OOPS";
+	}
+	let sum = 0;
+	let x = 0;
+	let y = 1;
+	for(i = num; i > 0; i--){
+		if(i == num){
+			sum = y;
+		} else {
+			sum = y + x;
+			x = y;
+			y = sum;
+		}
+	}
+	return sum;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
